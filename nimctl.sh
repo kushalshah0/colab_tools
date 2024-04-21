a9a79d3121a52f9e00eb0e33bf8a7a679b2da28f
Kushal Shah <code@kushalshah.com.np>
2024-04-21 13:32:32 +0545

    Create nimctl.sh

---
+++ nimctl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
