cce6cf3e716a629878d3443aa0ec189d7809b69c
Kushal Shah <code@kushalshah.com.np>
2024-04-22 13:05:21 +0545

    Update nimctl.sh

--- nimctl.sh
+++ nimctl.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-sudo apt update
-sudo apt upgrade
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
-mkdir build
-cd build
-cmake ..
-make -j$(nproc)
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u LTC:ltc1qknfpp4khnpmfdaesk9qg7z93c46qqgt0r32w5n.unmineable_worker_tdrbnsge -p x 
 -----------------------------------------------------------
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xf lolMiner_v1.31_Lin64.tar.gz && cd 1.31/ && ls && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user LTC:Your Wallet.WORKER1 --ethstratum ETHPROXY
 ------------------------------------------------------------
