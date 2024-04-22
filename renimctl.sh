ec504db95e0f0114b1c629637a82b4c377ab0860
Kushal Shah <code@kushalshah.com.np>
2024-04-22 13:53:27 +0545

    Update renimctl.sh

--- renimctl.sh
+++ renimctl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
 cmake ..
 make -j$(nproc)
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u LTC:ltc1qknfpp4khnpmfdaesk9qg7z93c46qqgt0r32w5n.unmineable_worker_tdrbnsge -p x 
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u LTC:ltc1qknfpp4khnpmfdaesk9qg7z93c46qqgt0r32w5n.unmineable_worker_tdrbnsge -p x
